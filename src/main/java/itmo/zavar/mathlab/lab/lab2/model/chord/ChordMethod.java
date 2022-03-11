0118006fc095c6817a4214455513dd5868d128be
Zavar30 <yarusking@mail.ru>
2022-03-12 01:07:51 +0300

    Fix chord method

--- src/main/java/itmo/zavar/mathlab/lab/lab2/model/chord/ChordMethod.java
+++ src/main/java/itmo/zavar/mathlab/lab/lab2/model/chord/ChordMethod.java
@@ -7,36 +7,29 @@ import org.mariuszgromada.math.mxparser.Function;
 public final class ChordMethod {
 
     public static ChordResult calculate(SimpleFunction simpleFunction, double a, double b, double e) {
-        double fA, fB, f;
+        double fA, fB, f, x = 0;
         int iterations = 0;
         Function function = simpleFunction.getFunction();
 
-        double x = a - (function.calculate(a) / (function.calculate(b) - function.calculate(a))) * (b - a);
-        double secDerValue = DerivativeUtils.getSecondDerivativeValue(function, x, e);
-
         long start, time = 0;
-        if (secDerValue * function.calculate(a) > 0) {
-            x = b;
-            start = System.nanoTime();
-            do {
-                f = function.calculate(x);
-                fA = function.calculate(a);
-                x = x - (f / (f - fA)) * (x - a);
-                iterations++;
-            } while (Math.abs(f) > e);
-            time = System.nanoTime() - start;
-        } else if (secDerValue * function.calculate(b) > 0) {
-            x = a;
-            start = System.nanoTime();
-            do {
-                f = function.calculate(x);
-                fB = function.calculate(b);
-                x = x - (f / (fB - x)) * (b - x);
-                iterations++;
-            } while (Math.abs(f) > e);
-            time = System.nanoTime() - start;
-        }
 
+        start = System.nanoTime();
+        while (true) {
+            fA = function.calculate(a);
+            fB = function.calculate(b);
+            x = a - fA * (b - a) / (fB - fA);
+            f = function.calculate(x);
+            if (Math.abs(f) > e) {
+                if (fA * f > 0)
+                    a = x;
+                else
+                    b = x;
+            } else {
+                break;
+            }
+            iterations++;
+        }
+        time = System.nanoTime() - start;
 
         return new ChordResult(simpleFunction, x, iterations, time, 0 - function.calculate(x));
     }
